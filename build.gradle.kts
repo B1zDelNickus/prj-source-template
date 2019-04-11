16ef78522841777418109eda168894bc847d62de
comdiv <fagim.sadykov@gmail.com>
2019-04-12 01:09:31 +0500

    start work on source-setup

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,18 +1,34 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
 // версия может быть перекрыта при вызове параметром `publish_version`
 val version = "1.0.0"
 spectrumMultimodule(version) {
     commonDependency(project(":model"))
 }
-
-
-tasks.register("set-source-name") {
+val checkSourceInitialized = tasks.register("check-source-initialized") {
     group = "sources"
     doFirst {
-        val sourceName = (rootProject.findProperty("source-name") ?: rootProject.name).toString()
+        val props = File(rootProject.projectDir, "gradle.properties")
+        if (!props.exists()) throw Exception("File gradle.properties not exists")
+        if (props.readText().contains("~")) throw Exception("File gradle.properties not fully defined")
+    }
+}
+
+val setupSource = tasks.register("setup-source") {
+    group = "sources"
+    dependsOn(checkSourceInitialized)
+    doFirst {
+        val sourceName = rootProject.ensureProperty("source-name")
         val packageName = sourceName.replace("-", "_")
         subprojects {
             this.ensurePackage("codes.spectrum.sources.$packageName.$name")
         }
+    }
+}
+
+subprojects {
+    tasks.withType<KotlinCompile>() {
+        dependsOn(checkSourceInitialized)
     }
 }
 
