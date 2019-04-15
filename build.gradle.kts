39191146ab91140fac331b3bfe94175c0ba94ac0
comdiv <fagim.sadykov@gmail.com>
2019-04-15 14:03:18 +0500

    cleanup source template gradle task added

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,36 @@
 import sources.sourceProject
 // версия может быть перекрыта при вызове параметром `publish_version`
 sourceProject(ensureProperty("publish-version","1.0.0"))
+
+val istemplate = rootProject.name == "source"
+
+if(istemplate){
+    fun forceCleanDirectory(dir:File){
+        for(f in dir.listFiles()){
+            if(f.isDirectory){
+                forceCleanDirectory(f)
+            }else{
+                f.delete()
+            }
+        }
+        dir.delete()
+    }
+    val reset_template = tasks.register("reset-template"){
+        group = "sources"
+        doFirst{
+            File("./source.json").writeText("""{
+  "code":"",
+  "name":""
+}""")
+            File("./README.md").writeText(File("./buildSrc/src/main/kotlin/sources/README.md").readText())
+            for(d in arrayOf("test","model", "provider", "client", "service", "db", "transport", "agent")){
+                val dir = File("./${d}")
+                if(dir.exists()){
+                    forceCleanDirectory(dir)
+                    dir.mkdirs()
+                    File(dir,"build.gradle.kts").createNewFile()
+                }
+            }
+        }
+    }
+}
