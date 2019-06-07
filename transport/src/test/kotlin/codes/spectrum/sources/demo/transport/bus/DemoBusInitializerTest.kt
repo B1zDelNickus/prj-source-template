eba9f40c450a78b3bbe727d6fe952ef107dcee6b
Dmitriy Serobyan <serobyan.d@avtocod.ru>
2019-06-07 11:28:32 +0500

    (spectrum-sources-core) (S-7323) Тест для миграции bus

---
+++ transport/src/test/kotlin/codes/spectrum/sources/demo/transport/bus/DemoBusInitializerTest.kt
@@ -0,0 +1,50 @@
+package codes.spectrum.sources.demo.transport.bus
+
+import codes.spectrum.bus.integration.BusNavigator
+import codes.spectrum.bus.rabbit.BusRabbitInitializer
+import codes.spectrum.message.queue.getBodyAsString
+import codes.spectrum.sources.demo.DemoConstants
+import io.kotlintest.shouldBe
+import io.kotlintest.specs.StringSpec
+import org.springframework.amqp.rabbit.connection.CachingConnectionFactory
+import org.springframework.amqp.rabbit.core.RabbitTemplate
+
+/**
+ * Тест для миграции bus
+ */
+class DemoBusInitializerTest: StringSpec({
+
+    /**
+     * Замени параметры для миграции на prod или stage
+     * Локально можно запустить так
+     * docker run -p 5672:5672 -e RABBITMQ_DEFAULT_USER=test -e RABBITMQ_DEFAULT_PASS=test rabbitmq:3
+     */
+    "bus initialize".config(enabled = false) {
+        val host = "localhost"
+        val port = 5672
+        val user = "test"
+        val password = "test"
+
+        val connectionFactory = CachingConnectionFactory(host, port).apply {
+            setUsername(user)
+            setPassword(password)
+        }
+
+        BusRabbitInitializer(connectionFactory, sourceBus, true)
+
+        val rabbitTemplate = RabbitTemplate(connectionFactory)
+        val busNavigator = BusNavigator(sourceBus, rabbitTemplate)
+
+        val messageToSend = "message"
+
+        busNavigator.send(DemoConstants.DEFAULT_BUS_SERVICE_NAME, DemoConstants.DEFAULT_BUS_SERVICE_OUTPUT_NAME, messageToSend)
+        println("Послано $messageToSend")
+        val readMessage = busNavigator.read(DemoConstants.DEFAULT_BUS_SERVICE_NAME, DemoConstants.DEFAULT_BUS_SERVICE_INPUT_NAME).getBodyAsString(charSet = "UTF-8")
+        println("Прочитано $readMessage")
+
+        readMessage shouldBe messageToSend
+    }
+
+
+
+})
