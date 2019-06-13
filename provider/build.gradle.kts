dcd80a81cf0c54b4093c759e77ed45eb03bf7ba5
Dmitriy Serobyan <serobyan.d@avtocod.ru>
2019-06-13 17:11:22 +0500

    (spectrum-templates-source) (S-7323) Удаление зависимостей от konveyor

--- provider/build.gradle.kts
+++
@@ -1,6 +0,0 @@
-val spectrum_legacy_version = property("spectrum-version") as String
-
-dependencies {
-    // TODO (Конвеер должен подтягиваться из core, убрать когда заработает)
-    implementation("codes.spectrum:konveyor:0.1.8")
-}
