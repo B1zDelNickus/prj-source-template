932dcaca6142bf70c3b189dbc9b2d80ec5b8e37a
Dmitriy Serobyan <serobyan.d@avtocod.ru>
2019-06-13 16:48:06 +0500

    (spectrum-templates-source) (S-7323) TODO для зависимости от konveyor

--- provider/build.gradle.kts
+++ provider/build.gradle.kts
@@ -1,5 +1,6 @@
 val spectrum_legacy_version = property("spectrum-version") as String
 
 dependencies {
+    // TODO (Конвеер должен подтягиваться из core, убрать когда заработает)
     implementation("codes.spectrum:konveyor:0.1.8")
 }
