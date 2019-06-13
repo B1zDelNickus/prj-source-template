b47b5876be43fe801237a9f36b7ccd0f4946da92
Dmitriy Serobyan <serobyan.d@avtocod.ru>
2019-06-13 12:56:41 +0500

    (spectrum-sources-core) (S-7323) Навигатор вместе со структурой bus

--- agent/src/main/kotlin/codes/spectrum/sources/demo/agent/DemoBusConfig.kt
+++ agent/src/main/kotlin/codes/spectrum/sources/demo/agent/DemoBusConfig.kt
@@ -1,30 +1,12 @@
 package codes.spectrum.sources.demo.agent
 
 import codes.spectrum.bus.builder.bus
-import codes.spectrum.bus.integration.BusNavigator
-import codes.spectrum.message.queue.RabbitConstants
 import codes.spectrum.message.queue.toMessage
-import codes.spectrum.sources.config.GlobalConfig
-import codes.spectrum.sources.config.getOrDefault
 import codes.spectrum.sources.core.agent.AgentMessageTask
 import codes.spectrum.sources.demo.DemoConstants
-import codes.spectrum.sources.demo.transport.bus.sourceBus
-import org.springframework.amqp.rabbit.connection.CachingConnectionFactory
+import codes.spectrum.sources.demo.transport.bus.navigator
 
 object DemoBusConfig {
-    private val host = GlobalConfig.getOrDefault("rabbitHost", RabbitConstants.DEFAULT_PROD_HOST)
-    private val port = GlobalConfig.getOrDefault("rabbitPort", RabbitConstants.DEFAULT_PROD_PORT)
-    private val user = GlobalConfig.getOrDefault("rabbitUser", RabbitConstants.DEFAULT_PROD_USER)
-    private val password = GlobalConfig.getOrDefault("rabbitPassword", RabbitConstants.DEFAULT_PROD_PASS)
-
-    private val connectionFactory by lazy { CachingConnectionFactory(host, port).apply {
-            setUsername(user)
-            setPassword(password)
-        }
-    }
-
-    val navigator by lazy { BusNavigator(sourceBus, connectionFactory) }
-
     fun startBus() {
         bus {
             navigator(navigator)
