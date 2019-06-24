c5d89f74dc207463ed565c9816a49b6e14971e19
Nikita Anchugov <kookooroozzka@gmail.com>
2019-06-24 17:20:24 +0700

    (spectrum-internal-core) (S-7467) Обернул вызов runServer в try-catch

--- service/src/main/kotlin/codes/spectrum/sources/demo/service/Start.kt
+++ service/src/main/kotlin/codes/spectrum/sources/demo/service/Start.kt
@@ -1,8 +1,17 @@
 package codes.spectrum.sources.demo.service
 
+import codes.spectrum.sources.core.SourceDefinition
 import codes.spectrum.sources.core.service.runServer
+import org.slf4j.LoggerFactory
 
 
+val InstanceLog = LoggerFactory.getLogger(SourceDefinition.Instance.serviceLoggerName)
 fun main() {
-    runServer(module = module)
+    try {
+        runServer(module = module)
+    } catch(e:Throwable) {
+        InstanceLog.error("Error source ${SourceDefinition.Instance.name} (${SourceDefinition.Instance.code})",e)
+    } finally {
+        InstanceLog.info("Finish source ${SourceDefinition.Instance.name} (${SourceDefinition.Instance.code})")
+    }
 }
